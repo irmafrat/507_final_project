7b54a3cf86bacdfbe6a6dc8a885a58d4d0b5cc95
Irmarie Fraticelli-Rodriguez <irmafrat@umich.edu>
2020-04-29 21:31:49 -0400

    Backend and frontend working!

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,40 @@
 from flask import render_template, request
 from app import app
 from app.tweet_loader import Tweet_DB, DB_NAME
-ricky_db = Tweet_DB(DB_NAME)
+tweet_db = Tweet_DB(DB_NAME)
+
 
 @app.route('/')
 def index():
-    locations = ricky_db.get_user_locations()
-    languages = ricky_db.get_languages()
+    locations = tweet_db.get_user_locations()
+    languages = tweet_db.get_languages()
     return render_template("form.html", locations=locations, languages=languages)
 
 
-@app.route('/handle_form', methods=['POST'])
-def handle_the_form():
-    return render_template("response.html")
+# @app.route('/handle_form', methods=['POST'])
+# def handle_the_form():
+#     return render_template("response.html")
 
-#EXAMPLE CODE TO MANAGE REQUEST/POST FROM THE USER
+
+@app.route('/search/<page>/', methods=['GET'])
+def search_results(page):
+    page = int(page)
+    if request.method == 'GET':
+
+        total_pages= tweet_db.search_pages(search_query=request.args.get("search_query"), select_project=request.args.get("project"),
+                                           select_language=request.args.get("tweet_language"),select_location=request.args.get("tweet_location"))
+        if page > total_pages or page < 1:
+            return f"Page {page} does not exist for the given search."
+        else:
+            rows = tweet_db.search(search_query=request.args.get("search_query"), select_project=request.args.get("project"),
+                                           select_language=request.args.get("tweet_language"),select_location=request.args.get("tweet_location"),page= page)
+            return render_template("grid.html", page=page, total_pages=total_pages, rows=rows)
+
+
+
+
+
+# EXAMPLE CODE TO MANAGE REQUEST/POST FROM THE USER
 # @app.route('/success/<name>')
 # def success(name):
 #    return 'welcome %s' % name
